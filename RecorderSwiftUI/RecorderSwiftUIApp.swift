93a1c5dd06c8618cb099d7c94e8b2cca3ef64c7a
romanzhukovv <zhukov.roman89@gmail.com>
2022-03-11 23:06:02 +0300

    Initial Commit

---
+++ RecorderSwiftUI/RecorderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecorderSwiftUIApp.swift
+//  RecorderSwiftUI
+//
+//  Created by Roman Zhukov on 11.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct RecorderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
